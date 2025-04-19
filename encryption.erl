076a40e67193059963b9cf6adaba255f48d2338f
HenL188 <153272326+HenL188@users.noreply.github.com>
2025-04-18 23:37:10 -0600

    Add tag storge maybe

--- encryption.erl
+++ encryption.erl
@@ -6,7 +6,7 @@ key_gen() ->
     _ = crypto:rand_seed_alg(crypto_aes, Input),
     Length = iolist_size(Input),
     if
-        Length < 32 -> 
+        Length < 32 ->
             Bytes = 32 - Length,
             Rand = crypto:strong_rand_bytes(Bytes),
             R = binary_to_list(Rand),
@@ -24,8 +24,13 @@ encrypt(Key, Data) ->
     IV = <<0:128>>,
     AAD = "harpy",
     {Encrypt, Tag} = crypto:crypto_one_time_aead(aes_256_gcm, Key, IV, Data, AAD, true),
-    file:write_file("verify.txt",Tag, append),
-    Encrypt.
+    Tag2 = binary_to_list(Tag),
+    Tag3 = Tag2 ++ [10],
+    Verify = file:write_file("verify.txt", Tag3, [append]),
+    case Verify of
+        ok -> io:format("encrypted~n");
+        {error, Error} -> io:format("Error: ~w~n", [Error])
+    end.
 
 decrypt(Key, Data, Tag) ->
     IV = <<0:128>>,
@@ -33,5 +38,5 @@ decrypt(Key, Data, Tag) ->
     Decrypt = crypto:crypto_one_time_aead(aes_256_gcm, Key, IV, Data, AAD, Tag, false),
     case Decrypt of
         error -> io:format("Failed to decrypt");
-        _ -> io:format(Decrypt) 
+        _ -> io:format(Decrypt)
     end.
